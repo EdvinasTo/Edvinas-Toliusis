c7266e1f019726f5163d9656307a46eeccf485da
EdvinasTo <edvinas.toliusis1@gmail.com>
2026-03-23 17:36:47 +0200

    feature: configure tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
-import './App.css'
+import { motion } from 'framer-motion'
+
+const item = {
+	initial: { y: '2rem', opacity: 0 },
+	animate: {
+		y: 0,
+		opacity: 1,
+		transition: {
+			duration: 1,
+			ease: [0.2, 1, 0.4, 1] as const
+		}
+	}
+}
+
 function App() {
-	return <></>
+	return (
+		<>
+			<motion.main></motion.main>
+		</>
+	)
 }
 
 export default App
